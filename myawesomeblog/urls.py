246fb451aa3bc9ad6e8ac0c75b0f4b2df8218b38
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-10-21 21:28:56 +0500

    add Post and Event model

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+import eventsapp.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', eventsapp.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

80bc885b81c4000ed581faf0190ef5d7a462549c
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-10-22 17:13:43 +0500

    add static

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'myawesomeblog/static'
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
